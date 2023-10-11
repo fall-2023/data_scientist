bf45cdc1b83344c955f9ac77cc12ed449d68064e
Robert Sfarzo <114509292+rsfarzo@users.noreply.github.com>
2023-10-11 12:29:31 -0500

    refac ui for 3 regression types

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   #get "users/sign_out", destroy_user_session_path
+  get "/regression/linear", to: "regression#linear_regression"
+  get "/regression/multiple", to: "regression#multiple_regression"
+  get "/regression/gradient_descent", to: "regression#gradient_descent"
+
 end
